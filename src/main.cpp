e5acb48aa1446b66d08d6ba2f8285fee27f7676b
Simon <simonjoecornelius@gmail.com>
2022-04-09 03:03:24 +0200

    camera stuff i didn't quite understand all of it tbf

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,46 @@
 #include <stb_image.h>
 
 #include "shader/shader.h"
+#include "camera/camera.h"
 
+// settings
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = WIDTH / 2.0f;
+float lastY = HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// render
 bool isWireframe = false;
 float blend = 0.2f;
 float radius = 5.0f;
 
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
 void processInput(GLFWwindow* window) {
-    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    const float cameraSpeed = 2.5f * deltaTime;
+
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+    if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
         isWireframe = true;
-    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_RELEASE)
+    if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE)
         isWireframe = false;
+
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
 
     if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
         blend = (blend < 1.0f) ? blend + 0.01f : blend;
@@ -33,6 +63,30 @@ void processInput(GLFWwindow* window) {
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn) {
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
 
 int main() {
@@ -51,6 +105,9 @@ int main() {
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD" << std::endl;
@@ -156,12 +213,18 @@ int main() {
     shaders.Set("texture1", 0);
     shaders.Set("texture2", 1);
     while(!glfwWindowShouldClose(window)) {
-        processInput(window);
-        if (isWireframe)
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        else
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        // timing
+        auto currentFrame = static_cast<float> (glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
+
+        // input
+        processInput(window);
+
+
+        // update
+        // rotate cube
         shaders.Use();
         shaders.Set("blend", blend);
         glm::mat4 model(1.0f);
@@ -169,10 +232,8 @@ int main() {
         int modelLoc = glGetUniformLocation(shaders.ID, "model");
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-        glm::mat4 view(1.0f);
-        glm::vec3 cameraPos(glm::sin((float) glfwGetTime()) * radius, 0.0f, glm::cos((float) glfwGetTime()) * radius);
-        glm::vec3 cameraTarget(0.0f, 0.0f, 0.0f);
-        view = glm::lookAt(cameraPos, cameraTarget, glm::vec3(0.0f, 1.0f, 0.0f));
+        // camera
+        glm::mat4 view = camera.GetViewMatrix();
         int viewLoc = glGetUniformLocation(shaders.ID, "view");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
@@ -182,6 +243,11 @@ int main() {
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 
+        // render
+        if (isWireframe)
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        else
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -194,6 +260,7 @@ int main() {
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
+        // frame clean up
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
